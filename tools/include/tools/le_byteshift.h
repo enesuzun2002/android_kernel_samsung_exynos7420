ca8e3a5814c3982568daaa704a1682a11861e243
Christopher N. Hesse <raymanfx@gmail.com>
2015-02-22 15:43:59 +0100

    tools: extract linux type definitions to local header
    This allows for building the kernel on non-linux platforms.
    The patch was taken from official OpenWrt trunk releases.

--- tools/include/tools/le_byteshift.h
+++ tools/include/tools/le_byteshift.h
@@ -1,7 +1,11 @@
 #ifndef _TOOLS_LE_BYTESHIFT_H
 #define _TOOLS_LE_BYTESHIFT_H
 
+#ifdef __linux__
 #include <linux/types.h>
+#else
+#include "linux_types.h"
+#endif
 
 static inline __u16 __get_unaligned_le16(const __u8 *p)
 {
