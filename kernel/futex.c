0ca1303d5a8a7c085f5a463753f08c05d9dcd859
Jianyu Zhan <nasa4836@gmail.com>
2016-03-07 09:32:24 +0800

    futex: Replace barrier() in unqueue_me() with READ_ONCE()
    Commit e91467ecd1ef ("bug in futex unqueue_me") introduced a barrier() in
    unqueue_me() to prevent the compiler from rereading the lock pointer which
    might change after a check for NULL.
    
    Replace the barrier() with a READ_ONCE() for the following reasons:
    
    1) READ_ONCE() is a weaker form of barrier() that affects only the specific
       load operation, while barrier() is a general compiler level memory barrier.
       READ_ONCE() was not available at the time when the barrier was added.
    
    2) Aside of that READ_ONCE() is descriptive and self explainatory while a
       barrier without comment is not clear to the casual reader.
    
    No functional change.
    
    [ tglx: Massaged changelog ]
    
    Change-Id: I7051dd06e6f15dd3dcdcfb1e47b23200d8473f37
    Signed-off-by: Jianyu Zhan <nasa4836@gmail.com>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Acked-by: Darren Hart <dvhart@linux.intel.com>
    Cc: dave@stgolabs.net
    Cc: peterz@infradead.org
    Cc: linux@rasmusvillemoes.dk
    Cc: akpm@linux-foundation.org
    Cc: fengguang.wu@intel.com
    Cc: bigeasy@linutronix.de
    Link: http://lkml.kernel.org/r/1457314344-5685-1-git-send-email-nasa4836@gmail.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/futex.c
+++ kernel/futex.c
@@ -1681,8 +1681,7 @@ static int unqueue_me(struct futex_q *q)
 
 	/* In the common case we don't take the spinlock, which is nice. */
 retry:
-	lock_ptr = q->lock_ptr;
-	barrier();
+	lock_ptr = READ_ONCE(q->lock_ptr);
 	if (lock_ptr != NULL) {
 		spin_lock(lock_ptr);
 		/*
