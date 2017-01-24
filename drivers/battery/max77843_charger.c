c7fbd7ba55d1269c1c28fce7a9fadb371540cadc
Tkkg1994 <luca.grifo@outlook.com>
2017-01-24 16:25:47 +0100

    max77843: add unstable power detection
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- drivers/battery/max77843_charger.c
+++ drivers/battery/max77843_charger.c
@@ -2080,6 +2080,8 @@ static irqreturn_t max77843_aicl_irq(int irq, void *data)
 	return IRQ_HANDLED;
 }
 
+bool unstable_power_detection = true;
+
 static void max77843_chgin_isr_work(struct work_struct *work)
 {
 	struct max77843_charger_data *charger = container_of(work,
@@ -2119,7 +2121,7 @@ static void max77843_chgin_isr_work(struct work_struct *work)
 			stable_count++;
 		else
 			stable_count = 0;
-		if (stable_count > 10) {
+		if (stable_count > 10 || !unstable_power_detection) {
 			pr_info("%s: irq(%d), chgin(0x%x), chg_dtls(0x%x) prev 0x%x\n",
 					__func__, charger->irq_chgin,
 					chgin_dtls, chg_dtls, prev_chgin_dtls);
