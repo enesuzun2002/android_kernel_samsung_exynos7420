7008ce9b03b17fcae309e25e1112c459be5a3ad8
UpInTheAir <upintheair.skyhigh@gmail.com>
2017-03-08 15:39:08 +0700

    decon_core.c: fix compile with TRACING disabled
    Signed-off-by: UpInTheAir <upintheair.skyhigh@gmail.com>

--- drivers/video/exynos/decon/decon_core.c
+++ drivers/video/exynos/decon/decon_core.c
@@ -131,6 +131,7 @@ static atomic_t extra_vsync_wait;
 
 static void tracing_mark_write( int pid, char id, char* str1, int value )
 {
+#ifdef CONFIG_TRACING
 	char buf[80];
 
 	if(!pid) return;
@@ -150,6 +151,7 @@ static void tracing_mark_write( int pid, char id, char* str1, int value )
 	}
 
 	trace_puts(buf);
+#endif
 }
 /*-----------------------------------------------------------------*/
 
