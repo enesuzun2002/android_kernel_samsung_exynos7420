d3169234d439444a625dce14ed9ab00d2659248a
Christoph Lameter <cl@linux.com>
2017-08-01 01:28:43 +0200

    vmstat: Reduce time interval to stat update on idle cpu
    It was noted that the vm stat shepherd runs every 2 seconds and that the
    vmstat update is then scheduled 2 seconds in the future.
    
    This yields an interval of double the time interval which is not desired.
    
    Change the shepherd so that it does not delay the vmstat update on the
    other cpu.  We stil have to use schedule_delayed_work since we are using a
    delayed_work_struct but we can set the delay to 0.
    
    Signed-off-by: Christoph Lameter <cl@linux.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Cc: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1188,7 +1188,8 @@ static void __cpuinit start_cpu_timer(int cpu)
 	struct delayed_work *work = &per_cpu(vmstat_work, cpu);
 
 	INIT_DEFERRABLE_WORK(work, vmstat_update);
-	schedule_delayed_work_on(cpu, work, __round_jiffies_relative(HZ, cpu));
+	schedule_delayed_work_on(cpu,
+		&per_cpu(vmstat_work, cpu), 0);
 }
 
 /*
