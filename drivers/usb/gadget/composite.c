b3b5dbff230122e5f13fe8350ba0dba07e426112
Lukas0610 <mail@lukasberger.at>
2018-03-02 22:17:55 +0100

    usb: gadget: hack FunctionFS/SuperSpeed-updates into Samsung's USB-stack
    Original commit: https://github.com/LineageOS/android_kernel_motorola_msm8974/commit/1c30279966ed5e22f3cc12edd4d7a29010b551c6
    
    Required to at least make (post-)O-ADB without enabled legacy interface enabled.
    
    Hacked into Samsung's USB-stack without much thinking
    
    Change-Id: Ie7a303e3550588b576e0968cdd0b58bdb177c3af

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1329,7 +1329,7 @@ composite_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 					cdev->desc.bcdUSB = cpu_to_le16(0x0300);
 					cdev->desc.bMaxPacketSize0 = 9;
 				} else {
-					cdev->desc.bcdUSB = cpu_to_le16(0x0210);
+					cdev->desc.bcdUSB = cpu_to_le16(0x0201);
 				}
 			}
 
