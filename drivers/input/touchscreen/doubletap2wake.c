d7e95f7cb302eea8e374dc2305d101407f2dee21
Lukas0610 <mail@lukasberger.at>
2018-03-02 11:42:05 +0100

    input: dt2w: fix enabling/disabling DT2W
    Change-Id: Ied48dd6cec8b8e77aca39a1fb821554eb9ed79c1

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -350,10 +350,15 @@ static ssize_t dt2w_doubletap2wake_show(struct device *dev,
 static ssize_t dt2w_doubletap2wake_dump(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
-	if (buf[0] >= '0' && buf[0] <= '2' && buf[1] == '\n')
-                if (dt2w_switch != buf[0] - '0')
-		        dt2w_switch = buf[0] - '0';
+	int val = 0;
+	int ret = kstrtoint(buf, 0, &val);
+	if (ret < 0)
+		return ret;
 
+	if (val != 0 && val != 1)
+		return -EINVAL;
+
+	dt2w_switch = val;
 	dt2w_toggled = true;
 
 	return count;
