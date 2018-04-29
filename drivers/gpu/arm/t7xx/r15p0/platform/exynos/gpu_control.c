331ed34d7eb1d7cfa2ce4688ec7f8ed5ac0b3768
enesuzun2002 <4lukankalar@gmail.com>
2018-04-29 06:03:49 +0000

    gpu: Improve mininum GPU freq to 160mhz
    Change-Id: Ifff09815cd29c3722d0a4e4128bf59e3cf62273c

--- drivers/gpu/arm/t7xx/r15p0/platform/exynos/gpu_control.c
+++ drivers/gpu/arm/t7xx/r15p0/platform/exynos/gpu_control.c
@@ -41,7 +41,7 @@
 
 static struct gpu_control_ops *ctr_ops;
 
-unsigned int gpu_min_override = 100;
+unsigned int gpu_min_override = 160;
 unsigned int gpu_max_override = 700;
 
 #ifdef CONFIG_MALI_RT_PM
