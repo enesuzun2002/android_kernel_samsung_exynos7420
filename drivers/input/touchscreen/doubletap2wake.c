b458c320b908a68dfad3a84160e98695ba9e3170
enesuzun2002 <4lukankalar@gmail.com>
2018-06-11 07:46:26 +0000

    dt2w: Apply Patch to Fix Compile Error Thanks Lukas
    Change-Id: I80f447bc50ee5d42243f917bc5df3c9ca9f0f86e

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -19,6 +19,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#include <linux/delay.h>
 #include <linux/device.h>
 #include <linux/fb.h>
 #include <linux/init.h>
@@ -111,11 +112,11 @@ static bool dt2w_static_wakelock_enabled;
 /*
  * DT2W Global Stuff
  */
-bool dt2w_is_enabled() {
+bool dt2w_is_enabled(void) {
 	return dt2w_enabled;
 }
 
-bool dt2w_just_enabled() {
+bool dt2w_just_enabled(void) {
 	return dt2w_was_just_enabled;
 }
 
