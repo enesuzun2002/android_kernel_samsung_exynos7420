TeamSPR/kernel

bcmdhd: reduce rx wakelock timeout via sysfs node
/sys/module/bcmdhd/parameters/wl_divide

Ported from BCMDHD4358 https://github.com/UpInTheAir/SM-N915/commit/a8f52f8df37dc1e9eecbab6d45ef49a95b870f9f for BCM4359.

Original implementation and credit for BCMDHD @halaszk

Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

drivers/net/wireless/bcmdhd4359/dhd_linux.c
@@ -211,6 +211,11 @@ static u32 vendor_oui = CONFIG_DHD_SET_RANDOM_MAC_VAL;
 
 #include <wl_android.h>
 
+#include <linux/moduleparam.h>
+
+static int wl_divide = 1;
+module_param(wl_divide, int, 0644);
+
 /* Maximum STA per radio */
 #define DHD_MAX_STA     32
 
@@ -11042,10 +11047,10 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 #ifdef CONFIG_HAS_WAKELOCK
 		if (dhd->wakelock_rx_timeout_enable)
 			wake_lock_timeout(&dhd->wl_rxwake,
-				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable)/4);
+				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable)/wl_divide);
 		if (dhd->wakelock_ctrl_timeout_enable)
 			wake_lock_timeout(&dhd->wl_ctrlwake,
-				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable)/4);
+				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable)/wl_divide);
 #endif
 		dhd->wakelock_rx_timeout_enable = 0;
 		dhd->wakelock_ctrl_timeout_enable = 0;

enesuzun2002/android_kernel_samsung_exynos7420

Ported from BCMDHD4358 https://github.com/UpInTheAir/SM-N915/commit/a8f52f8df37dc1e9eecbab6d45ef49a95b870f9f for BCM4359,
ported it back to BCMDHD4358 afterwards...
Conflicts:
	drivers/net/wireless/bcmdhd4359/dhd_linux.c
Change-Id: Iac39a6487c563ed0af924d75d9a7cd9cbc073a3f

drivers/net/wireless/bcmdhd4358/dhd_linux.c
@@ -163,9 +163,13 @@ extern void dhd_dump_eapol_4way_message(char *dump_data, bool direction);
 
 #include <wl_android.h>
 
+#include <linux/moduleparam.h>
+
 /* Maximum STA per radio */
 #define DHD_MAX_STA     32
 
+static int wl_divide = 1;
+module_param(wl_divide, int, 0644);
 
 const uint8 wme_fifo2ac[] = { 0, 1, 2, 3, 1, 1 };
 const uint8 prio2fifo[8] = { 1, 0, 0, 1, 2, 2, 3, 3 };
@@ -10150,10 +10154,10 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 #ifdef CONFIG_HAS_WAKELOCK
 		if (dhd->wakelock_rx_timeout_enable)
 			wake_lock_timeout(&dhd->wl_rxwake,
-				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable));
+				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable) / wl_divide);
 		if (dhd->wakelock_ctrl_timeout_enable)
 			wake_lock_timeout(&dhd->wl_ctrlwake,
-				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable));
+				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable) / wl_divide);
 #endif
 		dhd->wakelock_rx_timeout_enable = 0;
 		dhd->wakelock_ctrl_timeout_enable = 0;